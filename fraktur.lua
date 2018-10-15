fab16015923aef979f11273a3d765a8d9f06b133
Hanno Behrens <behrens.hanno@gmail.com>
2018-10-15 21:19:16 +0200

    Fraktur update

--- fraktur.lua
+++ fraktur.lua
@@ -3,6 +3,9 @@ require"ccrypt"
 local fraktur="𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝖀𝖁𝖂𝖃𝖄𝖅"
 local alpha="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 local text=io.read("*a")
+local text=text:gsub("st", "𝖘𝖙")
+local text=text:gsub("(%a)ss(%a)", "%1𝖋𝖋%2")
+local text=text:gsub("(%a)s(%a)", "%1𝖋%2")
 local frak_tab=alpha:subst_table(fraktur)
 text=text:substitute(frak_tab)
 frak_tab={ä="𝖆̈", ö="𝖔̈", ü="𝖚̈", Ä="𝕬̈", Ö="𝕺̈", Ü="𝖀̈", ß="𝖋𝖟"}
