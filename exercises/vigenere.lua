62221fc6beb4338b5c3b6e40350493bdce91f727
Hanno Behrens <behrens.hanno@gmail.com>
2019-04-02 13:23:02 +0200

    vigenere

--- exercises/vigenere.lua
+++ exercises/vigenere.lua
@@ -1,43 +1,23 @@
 #!/usr/bin/env luajit
-function Encrypt( _msg, _key )    
-    local msg = { _msg:upper():byte( 1, -1 ) }
-    local key = { _key:upper():byte( 1, -1 ) }    
-    local enc = {}
- 
-    local j, k = 1, 1
-    for i = 1, #msg do    
-        if msg[i] >= string.byte('A') and msg[i] <= string.byte('Z') then
-            enc[k] = ( msg[i] + key[j] - 2*string.byte('A') ) % 26 + string.byte('A')
- 
-            k = k + 1
-            if j == #key then j = 1 else j = j + 1 end
-        end
-    end
- 
-    return string.char( unpack(enc) )
+function cesar(c, k)
+	if c < string.byte("A") or string.byte("Z") < c then
+		return c-65
+	end
+	return (c-65+k)%26
 end
- 
-function Decrypt( _msg, _key )
-    local msg = { _msg:byte( 1, -1 ) }
-    local key = { _key:upper():byte( 1, -1 ) }      
-    local dec = {}
- 
-    local j = 1
-    for i = 1, #msg do            
-       dec[i] = ( msg[i] - key[j] + 26 ) % 26 + string.byte('A')
- 
-       if j == #key then j = 1 else j = j + 1 end
-    end    
- 
-    return string.char( unpack(dec) )
+
+function vigenere(text, pass)
+	text=text:upper()
+	pass=pass:upper()
+	local str=""
+	local ip=1
+	for i=1,#text do
+		str=str..string.char(
+			65+cesar(text:byte(i), 
+				cesar(pass:byte(ip), 0)))
+		ip=(ip%#pass)+1
+	end
+	return str
 end
- 
- 
-original = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!"
-key = "VIGENERECIPHER";
- 
-encrypted = Encrypt( original, key )
-decrypted = Decrypt( encrypted, key )
- 
-print( encrypted )
-print( decrypted )
+
+print(vigenere(arg[1], arg[2]))
