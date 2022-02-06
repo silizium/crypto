b8451a45007a055ea10412b82017ef12d4ff8ef5
Hanno Behrens <behrens.hanno@gmail.com>
2022-02-06 09:16:41 +0100

    cwkoch, a small koch trainer for telegraphy

--- caesar.lua
+++ caesar.lua
@@ -5,21 +5,16 @@ require "ccrypt"
 -- oder -key für Entschlüsselung
 local key=arg[1] and tonumber(arg[1]) or 13
 local text=io.read("*a") -- STDIN einlesen
-local alphabet
-if arg[2] == null then
-	alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	-- wir wandeln erstmal unseren Text in Großbuchstaben
-	text=text:upper()
-	-- auch die Sonderzeichen 
-	local toupper_tab=("äöü"):subst_table("ÄÖÜ")
-	text=text:substitute(toupper_tab)
-	-- und jetzt wandeln wir die Sonderzeichen in ASCII
-	local enc_key={}
-	enc_key.ß="SZ" enc_key.Ä="AE" enc_key.Ö="OE" enc_key.Ü="UE"
-	text=text:substitute(enc_key)
-else
-	alphabet=arg[2]
-end
+local alphabet=arg[2]~=null and arg[2] or "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+-- wir wandeln erstmal unseren Text in Großbuchstaben
+text=text:upper()
+-- auch die Sonderzeichen 
+local toupper_tab=("äöü"):subst_table("ÄÖÜ")
+text=text:substitute(toupper_tab)
+-- und jetzt wandeln wir die Sonderzeichen in ASCII
+local enc_key={}
+enc_key.ß="SZ" enc_key.Ä="AE" enc_key.Ö="OE" enc_key.Ü="UE"
+text=text:substitute(enc_key)
 --[[ jetzt verschlüsseln wir ihn mit klassischem Cäsar
 wenn wir kein cipher-alphabet angeben, nimmt er das 
 ]]
