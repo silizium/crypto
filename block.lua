c81b27cc0d8d6b7401ee5eea8e5dba4f3de092ce
Hanno Behrens <behrens.hanno@gmail.com>
2022-06-24 18:41:26 +0200

    block does not filter punctuation by default anymore

--- block.lua
+++ block.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env luajit
 require "ccrypt"
 local block=arg[1] and tonumber(arg[1]) or 5
-local line=arg[2] and tonumber(arg[2]) or 60
+local line=arg[2] and tonumber(arg[2]) or 55 
 local pat=arg[3] or "[%s%c]+"
 
 local str=io.read("*a")
