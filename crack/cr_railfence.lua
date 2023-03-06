bd4f1e725d3f483e9aef9728a0bd041e3fc5be86
Hanno Behrens <behrens.hanno@gmail.com>
2023-03-06 13:50:42 +0100

    railfence cipher crack

--- crack/cr_railfence.lua
+++ crack/cr_railfence.lua
@@ -1,22 +1,5 @@
 #!/usr/bin/env luajit
 require "ccrypt"
--- Aufruf mit pipe_caesar <key>
--- oder -key für Entschlüsselung
-function caesar(text, key, alphabet)
-	enc_key=alphabet:subst_table(alphabet, key)
-	text=text:substitute(enc_key)
-	return text
-end
-
-function string.upperutf(text)
-	text=text:upper()
-	local toupper_tab=("äöü"):subst_table("ÄÖÜ")
-	text=text:substitute(toupper_tab)
-	local enc_key={}
-	enc_key.ß="SZ" enc_key.Ä="AE" enc_key.Ö="OE" enc_key.Ü="UE"
-	text=text:substitute(enc_key)
-	return text
-end
 
 local bigrams={["TH"]=3.56, ["HE"]=3.07, ["IN"]=2.43, ["ER"]=2.05, ["AN"]=1.99, 
 	["RE"]=1.85, ["ON"]=1.76, ["AT"]=1.49, ["EN"]=1.45, ["ND"]=1.35, ["TI"]=1.34, 
@@ -44,7 +27,7 @@ function gram_rating(text)
 
 	if #text<3 then return birate end
 	local trirate=0
-	-- trigrams
+	-- bigrams
 	for i=1,#text-2 do
 		local tri=text:sub(i,i+1)
 		local val=trigrams[tri]
@@ -55,29 +38,30 @@ function gram_rating(text)
 	return birate+trirate
 end
 
-local text=io.read("*a"):upperutf():gsub("[^%w]", "") -- read STDIN 
-local alphabet=arg[1]~=null and arg[1] or "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+local text=io.read("*a"):upper():umlauts():gsub("[^%w]", "") -- read STDIN 
 
 local max,max_key,max_decrypt=0,0,""
 local list={}
-for key=0,25 do
-	local decrypt=caesar(text,key,alphabet)
+for rails=1,#text do
+	local decrypt=text:railfence_decrypt(rails)
 	local rating=gram_rating(decrypt)
-	list[#list+1]={(26-key)%26,rating,decrypt}
+	list[#list+1]={rail=rails,weight=rating,text=decrypt}
 	if rating>max then
 		max=rating
 		max_decrypt=decrypt
-		max_key=(26-key)%26
+		max_key=rails
 	end
 end
 --sorted list
-table.sort(list, function(k1, k2) return k1[2]>k2[2] end)
+table.sort(list, function(a, b) return a.weight>b.weight end)
 --print the first best 30%
+
+local percent=arg[1] or tonumber(arg[1]) or 0.7
 for _,v in ipairs(list) do 
-	if v[2]<0.7*max then break end
-	io.stderr:write(string.format("%2d %1.2f %s", v[1], v[2], v[3]))
+	if v.weight<percent*max then break end
+	io.stderr:write(string.format("%2d %1.2f %s", v.rail, v.weight, v.decrypt))
 end
 --best guess
-io.stderr:write("Best guess - Key: ",max_key," rating: ",max, "\n")
+io.stderr:write("\nBest guess - Key: ",max_key," rating: ",max, "\n")
 io.write(max_decrypt)
 
