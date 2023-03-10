89b82dfe9c0f718bf794e5dca7f42516e97d11b4
Hanno Behrens <behrens.hanno@gmail.com>
2023-03-10 17:10:10 +0100

    first proof of concept of breaking enigma

--- ccrypt.lua
+++ ccrypt.lua
@@ -554,3 +554,215 @@ function string.codebook_decode(text,book)
 	return table.concat(dec)
 end
 
+
+-- ENIGMA
+-- Objektorientierte Lösung
+-- Rad Objekt
+local Rad={}
+Rad.__index = Rad
+setmetatable(Rad, {
+  __call = function (cls, ...)
+    return cls.new(...)
+  end,})
+function Rad.new(stellung, ring, coding)	-- Argument "A", "1" zum Beispiel
+	local self=setmetatable({}, Rad)
+	self.rad={
+--       ABCDEFGHIJKLMNOPQRSTUVWXYZ
+		"EKMFLGDQVZNTOWYHXUSPAIBRCJ",		-- Rotor 1
+		"AJDKSIRUXBLHWTMCQGZNPYFVOE",		-- Rotor 2
+		"BDFHJLCPRTXVZNYEIWGAKMUSQO",		-- Rotor 3
+		"ESOVPZJAYQUIRHXLNFTGKDCMWB",		-- Rotor 4
+		"VZBRGITYUPSDNHLXAWMJQOFECK",		-- Rotor 5
+		"JPGVOUMFYQBENHZRDKASXLICTW",		-- Rotor 6
+		"NZJHGRCXMYSWBOUFAIVLPEKQDT",		-- Rotor 7
+		"FKQHTLXOCBJSPDZRAMEWNIUYGV",		-- Rotor 8
+		["B"]="LEYJVCNIXWPBQMDRTAKZGFUHOS", -- Rotor "beta"
+		["G"]="FSOKANUERHMBTIYCWLQPZXVGJD", -- Rtoor "gamma"
+	}
+	self.knocktab={
+		-- Royal Flags Wave Kings Above (3xAN)
+		{18},{6},{23},{11},{1},{1,14},{1,14},{1,14}
+	}
+	self:set(stellung) -- Buchstaben->1-26
+	self:setring(ring) -- Ringstellung->0-25 (subtrahiert)
+	self.coding=self.rad[coding] or coding
+	self.knock=self.knocktab[tonumber(coding)] or {}
+	return self
+end
+function Rad:setring(ring)
+	self.ring=string.byte(ring)-string.byte("A")
+end
+function Rad:set(stellung)
+	self.stellung=string.byte(stellung)-string.byte("A")+1
+end
+function Rad:get()
+	return string.char(self.stellung+string.byte("A")-1)
+end
+function Rad:step(step)
+	self.stellung=self.stellung+step
+	if self.stellung > #self.coding then 
+		self.stellung=self.stellung-#self.coding
+	elseif self.stellung < 1 then
+		self.stellung=self.stellung+#self.coding
+	end
+	for i=1,#self.knock do
+		if self.stellung==self.knock[i] then
+			return step,self.stellung
+		end
+	end
+	return 0,self.stellung
+end
+function Rad:crypt(char)
+	local pos=self.stellung-self.ring+string.byte(char)-string.byte("A")
+	if pos>#self.coding then
+		pos=pos-#self.coding
+	elseif pos<1 then
+		pos=pos+#self.coding
+	end
+	char=self.coding:sub(pos,pos)
+	pos=string.byte(char)-self.stellung+self.ring+1
+	if pos>string.byte("Z") then
+		pos=pos-26
+	elseif pos<string.byte("A") then
+		pos=pos+26
+	end
+	return string.char(pos)
+end
+function Rad:rcrypt(char)
+	char=string.byte(char)+self.stellung-self.ring-1
+	if char<string.byte("A") then 
+		char=char+26
+	elseif char>string.byte("Z") then
+		char=char-26
+	end
+	pos=self.coding:find(string.char(char))-self.stellung+self.ring+string.byte("A")
+	if pos<string.byte("A") then
+		pos=pos+26
+	elseif pos>string.byte("Z") then
+		pos=pos-26
+	end
+	return string.char(pos)
+end
+
+-- Enigma Objekt
+Enigma={}
+Enigma.__index = Enigma
+setmetatable(Enigma, {
+  __call = function (cls, ...)
+    return cls.new(...)
+  end,})
+function Enigma.buildsubst(code)
+	-- "DE-HA-GI" -> {D="E", E="D", H="A", A="H", G="I", I="G"}
+	local steck={}
+	for c in ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"):gmatch("%u") do
+		steck[c]=c
+	end
+	for c1,c2 in code:gmatch("(%u)(%u)") do
+		steck[c1]=c2
+		steck[c2]=c1
+	end
+	return steck
+end
+function Enigma.new(password, options)
+	local self=setmetatable({}, Enigma)
+	self.ukwset={
+		Enigma.buildsubst("AE-BJ-CM-DZ-FL-GY-HX-IV-KW-NR-OQ-PU-ST"), -- alt, selten verwendet
+		Enigma.buildsubst("AY-BR-CU-DH-EQ-FS-GL-IP-JX-KN-MO-TZ-VW"), -- M3 B
+		Enigma.buildsubst("AF-BV-CP-DJ-EI-GO-HY-KR-LZ-MX-NW-QT-SU"), -- M3 C
+		Enigma.buildsubst("AE-BN-CK-DQ-FU-GY-HW-IJ-LO-MP-RX-SZ-TV"), -- M4 dünn B
+		Enigma.buildsubst("AR-BD-CO-EJ-FN-GT-HK-IV-LM-PW-QZ-SX-UY"), -- M5 dünn C
+		Enigma.buildsubst("AQ-BY-CH-DO-EG-FN-IV-JP-KU-LZ-MT-RX-SW"), -- Reichsbahn
+		Enigma.buildsubst("AI-BM-CE-DT-FG-HR-JY-KS-LQ-NZ-OX-PW-UV"), -- Schweizer
+		Enigma.buildsubst("AR-BU-CL-DQ-EM-FZ-GJ-HS-IY-KO-NT-PW-VX"), -- Abwehr
+	}
+	self.statorset={
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ",	-- Standard
+		"QWERTZUIOASDFGHJKPYXCVBNML",	-- Reichsbahn+Schweizer+Abwehr
+		"JWULCMNOHPQZYXIRADKEGVBTSF",	-- Enigma D
+	}
+	self.verbose=options
+	self.rad={}
+	password=password:upper()
+	local spruch,ring,ukw,walzen,stator,steck=password:match("(%u-),([%w-]-),(%u),(%w-),(%d),(.*)")
+	if not(spruch and ring and stator and walzen and ukw and steck) then help()	end
+
+	-- DEBUG
+	if self.verbose then io.stderr:write("Stellung: ",spruch," Ring: ",ring," UKW: ",ukw," Walzen: ",walzen," Stator: ",stator, " Stecker: ", steck, "\n") end
+	-- END DEBUG
+	if ring:match("%d") then
+		local a,b,c=ring:match("([%d]*)-([%d]*)-([%d]*)")
+		local d=ring:match("[%d]*-[%d]*-[%d]*-([%d]*)")
+		if d then
+			ring=string.char(64+a,64+b,64+c,64+d)
+		else
+			ring=string.char(64+a,64+b,64+c)
+		end
+	end
+	for i=1,#spruch do
+		walze=walzen:sub(i,i)
+		walze=tonumber(walze) or walze
+		self.rad[i]=Rad.new(spruch:sub(i,i),ring:sub(i,i),walze)
+	end
+	self.stator=Rad.new("A","A",self.statorset[tonumber(stator)])
+	self.ukw=tonumber(string.byte(ukw)-string.byte("A")+1)
+	self.steck=Enigma.buildsubst(steck)
+	self.doublestep=false
+	return self
+end
+function Enigma:crypt(text)
+	local v={}
+	for char in text:gmatch("%a") do
+		-- weiterschalten
+		local step=1
+		for i=#self.rad,#self.rad-2,-1 do
+			if i==#self.rad-1 then
+				for _,k in ipairs(self.rad[i].knock) do
+					local nextstop=self.rad[i].stellung+1
+					if nextstop>#self.rad[i].coding then 
+						nextstop=nextstop-#self.rad[i].coding 
+					end
+					if k == nextstop then
+						step=1
+					end
+				end
+			end
+			step=self.rad[i]:step(step)
+		end
+
+		if self.verbose then 
+			for i=1,#self.rad do
+				io.stderr:write(self.rad[i]:get())
+			end
+			io.stderr:write("=",char,":") 
+		end
+			-- Steckbrett
+			char=self.steck[char]
+		if self.verbose then io.stderr:write(char) end
+			-- Stator
+			char=self.stator:crypt(char)
+		if self.verbose then io.stderr:write(char) end
+			-- Hinweg
+			for i=#self.rad,1,-1 do
+				char=self.rad[i]:crypt(char)
+		if self.verbose then io.stderr:write(",R",i,"=",char) end
+			end
+			-- Reflektor
+			char=self.ukwset[self.ukw][char]
+		if self.verbose then io.stderr:write(",UKW=",char) end
+			-- Rückweg
+			for i=1,#self.rad do
+				char=self.rad[i]:rcrypt(char)
+		if self.verbose then io.stderr:write(",R",i,"=",char) end
+			end
+			-- Stator -1
+			char=self.stator:rcrypt(char)
+		if self.verbose then io.stderr:write(char) end
+			-- Steckbrett die zweite
+			char=self.steck[char]
+		if self.verbose then io.stderr:write(char,"\n") end
+		v[#v+1]=char
+	end
+	return table.concat(v)
+end
+
+
---
+++ crack/cr_enigma1.lua
@@ -0,0 +1,81 @@
+#!/usr/bin/env luajit
+require "ccrypt"
+
+local bigrams={["TH"]=3.56, ["HE"]=3.07, ["IN"]=2.43, ["ER"]=2.05, ["AN"]=1.99, 
+	["RE"]=1.85, ["ON"]=1.76, ["AT"]=1.49, ["EN"]=1.45, ["ND"]=1.35, ["TI"]=1.34, 
+	["ES"]=1.34, ["OR"]=1.28, ["TE"]=1.20, ["OF"]=1.17, ["ED"]=1.17, ["IS"]=1.13, 
+	["IT"]=1.12, ["AL"]=1.09, ["AR"]=1.07, ["ST"]=1.05, ["TO"]=1.05, ["NT"]=1.04,
+	["NG"]=0.95, ["SE"]=0.93, ["HA"]=0.93, ["AS"]=0.87, ["OU"]=0.87, ["IO"]=0.83, 
+	["LE"]=0.83, ["VE"]=0.83, ["CO"]=0.79, ["ME"]=0.79, ["DE"]=0.76, ["HI"]=0.76, 
+	["RI"]=0.73, ["RO"]=0.73, ["IC"]=0.70, ["NE"]=0.69,	["EA"]=0.69, ["RA"]=0.69, 
+	["CE"]=0.65,
+}
+local trigrams={
+	["THE"]=1.81, ["AND"]=0.73, ["THA"]=0.33, ["ENT"]=0.42, ["ING"]=0.72, 
+	["ION"]=0.42, ["TIO"]=0.31, ["FOR"]=0.34, ["OFT"]=0.22, ["STH"]=0.21,
+}
+function gram_rating(text)
+	if #text<2 then return 0 end
+	local birate=0
+	-- bigrams
+	for i=1,#text-1 do
+		local bi=text:sub(i,i+1)
+		local val=bigrams[bi]
+		birate=birate+(val and val or 0)
+	end
+	birate=birate/(#text-1)
+
+	if #text<3 then return birate end
+	local trirate=0
+	-- bigrams
+	for i=1,#text-2 do
+		local tri=text:sub(i,i+1)
+		local val=trigrams[tri]
+		trirate=trirate+(val and val or 0)
+	end
+	trirate=trirate/(#text-2)
+
+	return birate+trirate
+end
+
+local text=io.read("*a"):clean()
+
+local key,english,verbose=",1-1-1,B,123,1,",false,false
+key=arg[1] or key
+
+local max,max_key,max_decrypt=0,0,""
+local list={}
+local test="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for a in test:gmatch("%a") do
+for b in test:gmatch("%a") do
+for c in test:gmatch("%a") do
+	local radstellung=a..b..c
+	local enigma = Enigma.new(radstellung..key, verbose)
+	local decrypt=enigma:crypt(text)
+--io.stderr:write(radstellung, " ", decrypt, "\n")
+	local rating=gram_rating(decrypt)
+	list[#list+1]={stellung=radstellung,weight=rating,text=decrypt}
+	if rating>max then
+		max=rating
+		max_decrypt=decrypt
+		max_key=radstellung
+	end
+end end end
+--sorted list
+table.sort(list, function(a, b) return a.weight>b.weight end)
+--print the first best 30%
+
+local percent=arg[2] or tonumber(arg[2]) or 0.7
+for _,v in ipairs(list) do 
+	if v.weight<percent*max then break end
+	io.stderr:write(string.format("%s %1.2f %s\n", v.stellung, v.weight, v.text))
+end
+--best guess
+io.stderr:write("\nBest guess - Key: ",max_key," rating: ",max, "\n")
+io.write(max_decrypt)
+
+
+
+
+
+--time luajit -e 'i=1 txt="ABCDEFGHIJKLMNOPQRSTUVWXYZ" for a in txt:gmatch("%a") do for b in txt:gmatch("%a") do for c in txt:gmatch("%a") do io.write(i,":",a,b,c, " ") i=i+1 end end end '
