a9f5ea446e1eaf838af8af3744c1db48ab981004
Hanno Behrens <behrens.hanno@gmail.com>
2025-08-20 07:48:18 +0200

    added more increment options for Vigenère

--- vigenere.lua
+++ vigenere.lua
@@ -15,14 +15,19 @@ function string.vigenere_encrypt(text,password,alphabet,inc)
 	local v,t={}
 	t=vigenere_make(password,alphabet)
 	-- encode vigenere
-	local r,i=0,0
+	local r,i,nr=0,0,0
 	for c in text:gmatch(".") do
 		local from,to=alphabet:find(c)
 		if from then 
+			nr=nr+1
 			from=(from+i-1)%#alphabet+1
 			v[#v+1]=t[r+1]:sub(from,from) 
 			r=(r+1)%#password
-			i=i+inc
+			for _,move in ipairs(inc) do
+				if nr>move.offset and (nr-move.offset)%move.step==0 then
+					i=i+move.increment
+				end
+			end
 		else 
 			v[#v+1]=c
 		end
@@ -34,14 +39,19 @@ function string.vigenere_decrypt(text,password,alphabet,inc)
 	local v,t={}
 	t=vigenere_make(password,alphabet)
 	-- decode vigenere
-	local r,i=0,0
+	local r,i,nr=0,0,0
 	for c in text:gmatch(".") do
 		local from,to=t[r+1]:find(c)
 		if from then
+			nr=nr+1
 			from=(from+i-1)%#alphabet+1
 			v[#v+1]=alphabet:sub(from,from)
 			r=(r+1)%#password
-			i=i-inc
+			for _,move in ipairs(inc) do
+				if nr>move.offset and (nr-move.offset)%move.step==0 then
+					i=i-move.increment
+				end
+			end
 		else
 			v[#v+1]=c
 		end
@@ -50,7 +60,7 @@ function string.vigenere_decrypt(text,password,alphabet,inc)
 end
 
 local alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-local password,decrypt,randomize,clean,inc="SECRET",false,false,true,0
+local password,decrypt,randomize,clean,inc="SECRET",false,false,true,{}
 local fopt={
 	["h"]=function(optarg,optind) 
 		io.stderr:write(
@@ -62,7 +72,9 @@ local fopt={
 			.."-p	password (%s) \n\taccepts all that are in alphabet\n"
 			.."-r	randomize (%s) \n\taccepts \"time\" or seed number\n"
 			.."-c	not clean text from non alphabet characters\n"
-			.."-i	increment after each step\n"
+			.."-i	increment after each step <nr>s<steps>o<off>[,more]\n"
+			.."\tincrement nr amount after steps starting with offset\n"
+			.."\texample 1=every time 2s3 every thirt step by 2\n"
 			.."-d	decrypt (%s)\n",
 			arg[0], alphabet, password, randomize, decrypt)
 		)	
@@ -94,7 +106,13 @@ local fopt={
 		clean=not clean
 	end,
 	["i"]=function(optarg, optind)
-		inc=tonumber(optarg)
+		for arg in optarg:gmatch("([%dso]+)") do
+			local increment,step,offset=arg:match("(%d+)s*(%d*)o*(%d*)")
+			inc[#inc+1]={
+				increment=tonumber(increment),
+				step=step and tonumber(step) or 1,
+				offset=offset and tonumber(offset) or 0}
+		end
 	end,
 	["?"]=function(optarg, optind)
 		io.stderr:write(string.format("unrecognized option %s\n", arg[optind -1]))
